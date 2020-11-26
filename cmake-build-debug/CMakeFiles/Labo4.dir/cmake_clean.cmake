1b4d77da2bd2ec4ac0c36180a8060d7fb01278ab
Luca Coduri <coduri.vitor@gmail.com>
2020-11-26 11:12:35 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/Labo4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labo4.dir/main.cpp.obj"
+  "Labo4.exe"
+  "Labo4.exe.manifest"
+  "Labo4.pdb"
+  "libLabo4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Labo4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier : <nom du fichier>.<xxx> (xxx = h ou cpp)
+Auteur(s) : Luca Coduri
+Date creation : <jj.mm.aaaa>
+Description : <à compléter>
+Remarque(s) : <à compléter>
+Compilateur : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+*/
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   return EXIT_SUCCESS;
+}
