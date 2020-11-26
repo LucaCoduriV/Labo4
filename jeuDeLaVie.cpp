4c02bf11df75c6312ebe092553c04066c6bf8691
Luca Coduri <coduri.vitor@gmail.com>
2020-11-26 15:08:08 +0100

    création de la fonction etatSuivantCellule et calculeGenSuivante

--- jeuDeLaVie.cpp
+++ jeuDeLaVie.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "jeuDeLaVie.h"
+#include <array>
+
+using namespace std;
+
+bool etatSuivantCellule(char etatActuel, int nbVivant){
+   return (etatActuel == '.' && nbVivant == 3) || (etatActuel == 'X' && (nbVivant == 3 || nbVivant == 2));
+}
+
+array<array<char, 11>,10> calculeGenSuivante(const array<array<char, 11>,10>&
+   genActu){
+
+   array<array<char, 11>,10> genSuiv{};
+
+   for(size_t i = 0; i < genActu.size(); i++ ){
+      for(size_t j = 0; j < genActu.at(i).size(); j++ ){
+         if(etatSuivantCellule(genActu.at(i).at(j), 5) ){
+            genSuiv.at(i).at(j) = 'X';
+         }else{
+            genSuiv.at(i).at(j) = '.';
+         }
+      }
+   }
+   return genSuiv;
+}
--- jeuDeLaVie.h
+++ jeuDeLaVie.h
@@ -4,5 +4,8 @@
 
 #ifndef LABO4_JEUDELAVIE_H
 #define LABO4_JEUDELAVIE_H
-
+#include<array>
+bool etatSuivantCellule(char etatActuel, int nbVivant);
+std::array<std::array<char, 11>,10> calculeGenSuivante(const
+std::array<std::array<char, 11>,10>& genActu);
 #endif //LABO4_JEUDELAVIE_H
